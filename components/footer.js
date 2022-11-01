33236cc489a2f45371eb7ade3d30e69987e7881e
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2022-11-01 14:27:39 -0500

    Added 'Categories' component and 'Footer' component

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,29 @@
+import Link from "next/link";
+import { SiGithub, SiHackthebox, SiTwitter } from "react-icons/si";
+
+import Newsletter from "./_child/newsletter";
+
 export default function footer() {
+  const bg = {
+    backgroundImage: "url('/images/footer.png')",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "bottom left"
+  }
+
   return (
-    <div className="text-3xl">footer</div>
+    <footer className="bg-gray-50" style={bg}>
+      <Newsletter />
+      <div className="container mx-auto flex justify-center py-12">
+        <div className="py-5">
+          <div className="flex gap-6 justify-center">
+            <Link href={"https://github.com"}><SiGithub color="#888"/></Link>
+            <Link href={"/"}><SiHackthebox color="#888"/></Link>
+            <Link href={"/"}><SiTwitter color="#888"/></Link>
+          </div>
+          <p className="py-5 text-gray-400">Copyright ©2022 All rights reserved | This template is made with &hearts;</p>
+          <p className="text-gray-400 text-center">Terms & Condition</p>
+        </div>
+      </div>
+    </footer>
   )
 }
--- components/section4.js
+++ components/section4.js
@@ -3,20 +3,32 @@ import Link from "next/link"
 
 import Author from "./_child/author"
 
-export default function section2() {
+export default function section4() {
   return (
-    <section className="container mx-auto md:px-20 py-10">
-      <h1 className="font-bold text-4xl py-12 text-center">
-        Latest Post
-      </h1>
-
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
-        {Post()}
+    <section className="container mx-auto md:px-20 py-16">
+      <div className="grid lg:grid-cols-2">
+        <div className="item">
+          <h1 className="font-bold text-4xl py-12">
+            Business
+          </h1>
+          <div className="flex flex-col gap-6">
+            {Post()}
+            {Post()}
+            {Post()}
+            {Post()}
+          </div>
+        </div>
+        <div className="item">
+          <h1 className="font-bold text-4xl py-12">
+            Travel
+          </h1>
+          <div className="flex flex-col gap-6">
+            {Post()}
+            {Post()}
+            {Post()}
+            {Post()}
+          </div>
+        </div>
       </div>
     </section>
   )
@@ -24,19 +36,19 @@ export default function section2() {
 
 function Post() {
   return(
-    <div className="item">
-      <div className="images">
+    <div className="flex gap-5">
+      <div className="image flex flex-col justify-start">
         <Link href={"/"}>
           <Image
             src={"/images/img1.jpg"}
-            width={500}
-            height={350}
+            width={300}
+            height={250}
             alt={"image blog description"}
             className="rounded"
           />
         </Link>
       </div>
-      <div className="info flex justify-center flex-col py-4">
+      <div className="info flex justify-center flex-col">
         <div className="cat">
           <Link href={"/"} className="text-orange-600 hover:text-orange-800">
             Business, Travel&nbsp;
@@ -50,10 +62,6 @@ function Post() {
             Your most unhappy customers are your greatest source of learning 
           </Link>
         </div>
-        <p className="text-gray-500 py-3">
-          Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
-          text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
-        </p>
         <Author />
       </div>
     </div>
