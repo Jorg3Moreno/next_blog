8701a22542d339df2916cb274e751950d87a282a
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2022-10-31 19:21:10 -0500

    added Author component

---
+++ components/_child/author.js
@@ -0,0 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function author() {
+  return (
+    <div className="author flex py-5">
+      <Image
+        src={"/images/author/author1.jpg"}
+        width={60}
+        height={60}
+        alt="image author"
+        className="rounded-full"
+      />
+      <div className="flex flex-col justify-center px-4">
+        <Link href={"/"} className="font-bold text-gray-800 hover:text-gray-600">
+          Jorge Moreno
+        </Link>
+        <span className="text-sm text-gray-500">Developer and Hacker</span>
+      </div>
+    </div>
+  )
+}
