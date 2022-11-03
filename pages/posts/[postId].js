923cf6809d501487d5088f04cbc079cb8ca5a8fe
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2022-11-03 16:17:27 -0500

    Added 'SWRConfig' to cache data on page

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,12 +1,32 @@
 import Image from "next/image";
+import { SWRConfig } from "swr";
 
 import Format from "../../layout/format";
 import Author from "../../components/_child/author";
 import Related from "../../components/_child/related";
+import Spinner from "../../components/_child/spinner";
+import Error from "../../components/_child/error";
 
+import { useRouter } from "next/router";
 import { getAllPosts, getPost } from "../../lib/helper";
+import useFetcher from "../../lib/fetcher";
 
-export default function Page(post) {
+export default function Page({ fallback }) {
+  const router = useRouter();
+  const { postId } = router.query;
+
+  const { isError, isLoading, data } = useFetcher(`/api/posts/${postId}`);
+  if (isLoading) return <Spinner />;
+  if (isError) return <Error />;
+
+  return (
+    <SWRConfig value={{ fallback }}>
+      <Article post={data} />;
+    </SWRConfig>
+  );
+}
+
+function Article({ post }) {
   return (
     <Format>
       <section className="container mx-auto md:px-2 py-16 w-1/2">
@@ -14,7 +34,9 @@ export default function Page(post) {
           {post.author ? <Author /> : <></>}
         </div>
         <div className="post py-10">
-          <h1 className="font-bold text-4xl text-center pb-5">{post.title || "No title"}</h1>
+          <h1 className="font-bold text-4xl text-center pb-5">
+            {post.title || "No title"}
+          </h1>
           <p className="text-gray-500 text-xl text-center">
             {post.subtitle || "No subtitle"}
           </p>
@@ -27,9 +49,7 @@ export default function Page(post) {
             />
           </div>
           <div className="content text-gray-600 text-lg flex flex-col gap-45">
-            <p>
-              {post.description || "No description for this post"}
-            </p>
+            <p>{post.description || "No description for this post"}</p>
           </div>
         </div>
         <Related />
@@ -42,7 +62,11 @@ export async function getStaticProps({ params }) {
   const post = await getPost(params.postId);
 
   return {
-    props: post,
+    props: {
+      fallback: {
+        "/api/posts": post,
+      },
+    },
   };
 }
 
